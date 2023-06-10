4185cf04e7e0218acf4ebe9c1ac8e8f9766c3527
GreatAjax <mlindholm2000@gmail.com>
2023-06-09 20:05:44 -0400

    asdf

---
+++ gui-user.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Example of how to start the GUI with custom arguments. In this case how to auto launch the browser:
+REM call gui.bat --inbrowser
+REM
+REM You can add many arguments on the same line
+REM
+call gui.bat --inbrowser server_port 7870
